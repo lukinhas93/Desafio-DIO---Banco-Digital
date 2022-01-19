6a026d4fd530017e40a64457901fa12467ea9f17
lukinhas93 <lucas_ferreira_93@hotmail.com>
2022-01-19 01:39:33 -0300

    First commit

---
+++ src/ProjetoBancoDigital/Main.java
@@ -0,0 +1,19 @@
+package ProjetoBancoDigital;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cliente venilton = new Cliente();
+        venilton.setNome("Lucas");
+
+        Conta cc = new ContaCorrente(venilton);
+        Conta poupanca = new ContaPoupanca(venilton);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+
+}
